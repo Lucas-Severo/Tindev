0dfe638df22ff68db5d344ed1c86555600a17196
Lucas-Severo <lucas_severo@outlook.com.br>
2020-03-24 11:54:33 -0300

    implemented match screen in frontend

--- backend/src/controllers/LikeController.js
+++ backend/src/controllers/LikeController.js
@@ -2,6 +2,8 @@ const Dev = require('../models/Dev');
 
 module.exports = {
     async store(req, res) {
+        console.log(req.io, req.connectedUsers);
+
         const { user } = req.headers;
         const { devId } = req.params;
 
@@ -13,7 +15,16 @@ module.exports = {
         }
 
         if (targetDev.likes.includes(loggedDev._id)) {
-            console.log('Deu match');
+            const loggedSocket = req.connectedUsers[user];
+            const targetSocket = req.connectedUsers[devId];
+
+            if (loggedDev) {
+                req.io.to(loggedSocket).emit('match', targetDev);
+            }
+
+            if (targetDev) {
+                req.io.to(targetSocket).emit('match', loggedDev);
+            }
         }
 
         loggedDev.likes.push(targetDev._id);
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,37 @@
 const express = require('express');
 const router = require('./routes');
 const mongoose = require('mongoose');
+
 const cors = require('cors');
+require('dotenv').config();
 
-const server = express();
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
+const connectedUsers = {
+}
 
+io.on('connection', socket => {
+  const { user } = socket.handshake.query;
 
-mongoose.connect('yourLink', 
+  connectedUsers[user] = socket.id;
+});
+
+mongoose.connect(process.env.MONGO_URL, 
                 { useNewUrlParser: true,
                   useUnifiedTopology: true
                 });
 
-server.use(cors());
-server.use(express.json());
-server.use(router);
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
 
-server.listen(3333);
+  return next();
+})
+
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
+server.listen(process.env.PORT || 3333);
