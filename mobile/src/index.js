0c8e998268c50562cd213e3ce09c365adada0a73
Lucas-Severo <lucas_severo@outlook.com.br>
2020-03-24 00:11:22 -0300

    implemented mobile APP

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Text } from 'react-native';
+
+import Routes from './routes';
 
 export default function App() {
   return (
-    <Text>Hello world</Text>
+    <Routes />
   );
 }
