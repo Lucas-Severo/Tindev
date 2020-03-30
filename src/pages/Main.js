88a476a1c6641510710348199629b56bd219fabe
Lucas-Severo <lucas_severo@outlook.com.br>
2020-03-30 12:04:32 -0400

    update websocket route

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -29,7 +29,7 @@ export default function Main({ match }) {
     }, [match.params.id])
 
     useEffect(() => {
-        const socket = io('http://localhost:3333', {
+        const socket = io('https://api-tindevep.herokuapp.com/', {
             query: { user: match.params.id }
         });
 
